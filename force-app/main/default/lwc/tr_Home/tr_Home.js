95aafe310c338a660641371103b11b378c870ad8
DanLeg <daniel.legawiec@britenet.com.pl>
2021-08-16 15:23:57 +0200

    Add participants configurator

--- force-app/main/default/lwc/tr_Home/tr_Home.js
+++ force-app/main/default/lwc/tr_Home/tr_Home.js
@@ -1,10 +1,33 @@
 import { LightningElement } from 'lwc';
 import noHeaderAppPage from '@salesforce/resourceUrl/noHeaderAppPage'
 import { loadStyle } from 'lightning/platformResourceLoader';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+const NUMBER_OF_PLAYERS = 2;
 
 export default class Tr_Home extends LightningElement {
 
     connectedCallback() {
        loadStyle(this, noHeaderAppPage);
+    }
+
+    isNumberOfParticipantsGood() {
+        let participantComponent = this.template.querySelector('c-tr-_-participants-configurator');
+        if(participantComponent.selectedRows.length == NUMBER_OF_PLAYERS) {
+            return true;
+        }
+        return false;
+    }
+
+    handleGameStart() {
+        if(!this.isNumberOfParticipantsGood()) {
+            const toastEvent = new ShowToastEvent({
+                title: 'Number of participants must equals '+NUMBER_OF_PLAYERS,
+                varient: 'error'
+            });
+            this.dispatchEvent(toastEvent);
+            return;
+        }
+        
     }
 }
---
+++ force-app/main/default/lwc/tr_NewParticipant/tr_NewParticipant.js
@@ -0,0 +1,37 @@
+import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import cancel from '@salesforce/label/c.cancel'
+import submit from '@salesforce/label/c.submit'
+import close from '@salesforce/label/c.close'
+import new_participant from '@salesforce/label/c.new_participant'
+
+export default class Tr_NewParticipant extends LightningElement {
+
+    labels = {cancel, submit, close, new_participant}
+
+    handleClose() {
+        this.handleReset();
+        this.dispatchEvent(new CustomEvent('close'));
+    }
+
+    handleSuccess() {
+        const toastEvent = new ShowToastEvent({
+            title: 'Participant created',
+            varient: 'success'
+        });
+        this.dispatchEvent(toastEvent);
+        this.handleClose();
+        this.dispatchEvent(new CustomEvent('success'));
+    }
+
+    handleReset() {
+        const inputFields = this.template.querySelectorAll(
+            'lightning-input-field'
+        );
+        if (inputFields) {
+            inputFields.forEach(field => {
+                field.reset();
+            });
+        }
+     }
+}
